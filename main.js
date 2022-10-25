8bc5ae34d677ab910024ebb0e31ff6f4ee2f0d26
camila leal <khammylv@gmail.com>
2022-10-25 16:10:42 -0500

    Updates

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import ghibli from "./data/ghibli/ghibli.js";
+
 const objetos = ghibli
 
 import { mostrarPeliculaIndividual, filtrarDirectores,ordenarYears,ordenarScore, estadisticaData} from './data.js';
@@ -9,10 +10,9 @@ const cerrar = document.getElementById("close");
 const home = document.getElementById("home");
 const pagina = document.getElementById("muestra");
 const personajes = document.getElementById("personajes");
-const location = document.getElementById("locacion");
-const vehiculos = document.getElementById("vehiculos");
 const fecha = document.getElementById("fecha");
 const popular = document.getElementById("popular");
+const header = document.getElementById("header_inicio")
 
 window.onload = () => {
   mostrarEnPantalla(datosTodasPeliculas());
@@ -64,8 +64,9 @@ function mostrarEnPantalla(data) {
       pelicula.innerHTML += `
                      <div class = "card_pelicula">
                         <img src="${array[i].poster}" alt="${array[i].title}">
-                        <div class = "textos_card"
-                           <p>${array[i].title}</p>
+                       
+                        <div class = "textos_card">
+                           <h2>${array[i].title}</h2>
                            <p>director: ${array[i].director}</p>
                            <p>year: ${array[i].release_date}</p>
                         </div>
@@ -94,7 +95,7 @@ pelicula.addEventListener('mouseup', (e) => {
    if(abrir === "select"){
     
       modal.style.display = "block";
-      
+      header.style.display = "none"
    }
    
    
@@ -118,7 +119,7 @@ pelicula.addEventListener('mouseup', (e) => {
            <div class="modal_titulo_descripcion">
               <h2>${element.title}</h2>
               <div class = "modal_director"
-                <p>producer: ${element.producer}</p>
+                <p >producer: ${element.producer}</p>
                 <p>director: ${element.director}</p>
               </div>
               <div class="modal_texto_largo"  
@@ -133,43 +134,24 @@ pelicula.addEventListener('mouseup', (e) => {
           personajes.innerHTML = "";
           element.people.forEach(persona =>{
           personajes.innerHTML +=
-           `<div class = "cont-people">
-            <img src="${persona.img}" alt="${persona.name}">
-             <p>${persona.name}</p>
-             <p>${persona.specie}</p>
+           `<div class = "cont_people">
+              <img src="${persona.img}" alt="${persona.name}">
+              <div class ="con_people_text"
+                <p>Name: ${persona.name}</p>
+                <p>Specie: ${persona.specie}</p>
+              </div> 
            </div>
            ` 
           });
           
-          location.innerHTML = ""
-          element.locations.forEach(locacion =>{
-          
-            location.innerHTML +=
-            `<div class = "cont-locacion">
-               <img src="${locacion.img}" alt="${locacion.name}">
-               <p>${locacion.name}</p>
-               <p>${locacion.climate}</p>
-            </div>`
-
-          });
-          
-          vehiculos.innerHTML = ""
-          element.vehicles.forEach(vehiculo =>{
-            vehiculos.innerHTML +=
-            `<div class = "cont-vehiculos">
-            <img src="${vehiculo.img}" alt="${vehiculo.name}">
-            <p>${vehiculo.name}</p>
-            <p>${vehiculo.description}</p>
-            <p>${vehiculo.pilot.name}</p>
-                     
-            </div>`
-          });
+         
         });
       });
 
 
 cerrar.addEventListener("click", function() {
     modal.style.display = "none";
+    header.style.display = "block";
 });
 
 
@@ -184,7 +166,64 @@ popular.addEventListener("click",function(){
   const mostrarPopular = ordenarScore(datosTodasPeliculas());
   mostrarEnPantalla(mostrarPopular);
 }); 
+ 
 
 
-const estadistica = estadisticaData(datosTodasPeliculas());
-console.log(estadistica);
+
+
+
+
+
+estadisticasData();
+function estadisticasData(){
+  const estadistica = estadisticaData(datosTodasPeliculas());
+
+const imgDirect =[
+
+  "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e9/Gor%C5%8D_Miyazaki.jpg/220px-Gor%C5%8D_Miyazaki.jpg",
+  "https://upload.wikimedia.org/wikipedia/commons/e/e2/Hayao_Miyazaki_cropped_1_Hayao_Miyazaki_201211.jpg",
+   "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9c/Hiromasa_Yonebayashi.jpg/220px-Hiromasa_Yonebayashi.jpg",
+   "https://lthumb.lisimg.com/709/103709.jpg?width=280&sharpen=true",
+  "https://m.media-amazon.com/images/M/MV5BNDM5OTc2NjI1MV5BMl5BanBnXkFtZTgwOTI3MjA4MTE@._V1_.jpg",
+  "https://www.cartoonbrew.com/wp-content/uploads/2016/02/isaotakahata.jpg",
+]
+
+const img = [...imgDirect]
+  const estadistic = document.getElementById("contenedor_etadistica");
+  const estadisticImg = document.getElementById("cont_imagenes");
+  const nameDirec = document.getElementById("name_direc");
+ for(const [key, value] of Object.entries(estadistica)){
+   nameDirec.innerHTML += 
+   `<div class="directores_name">
+     <p>⭐${key}</p>
+   </div>`
+   
+    const porcentaje = (value * 100)/20;
+    estadistic.innerHTML +=
+
+   `
+      
+      <div class="barra" style="--bar-value:${porcentaje}%;" data-name="⭐${key}" title="${key} ${porcentaje}%"></div>
+    
+
+  `
+  //<div class="barra_name"></div>⭐${key}
+ }
+ for (let clave in img){
+  estadisticImg.innerHTML +=
+  `<div class="directores">
+  <img src="${img[clave]}" alt=""></img>
+  </div>
+  `
+}
+ 
+ 
+}
+  
+
+
+
+
+
+
+
